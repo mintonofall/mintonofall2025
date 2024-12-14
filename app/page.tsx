8fd160759827e89a193bc1b85794b8c61c43644e
mintonofall <mintonofall@gmail.com>
2024-12-14 10:39:40 +0900

    Add Prisma schema and integrate Pico CSS; update homepage and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import '@picocss/pico'
 
 export default function Home() {
   return (
   <div>
-    test
+    모두의 민턴에 오신걸 환영합니다.
+  <form>
+<label htmlFor="username">username</label>
+    <input id="username" name="username" type="text"/>
+    </form>
   </div>
   );
 }
