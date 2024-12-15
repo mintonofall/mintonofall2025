9d8a3ad55b63b6ff3c4c16cae9c7a7b82995c5e4
mintonofall <mintonofall@gmail.com>
2024-12-15 11:47:12 +0900

    Implement logout functionality and improve user session handling on the homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,35 @@
 import db from "@/lib/db";
-import '@picocss/pico'
+import "@picocss/pico";
 import getSession from "@/lib/session";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import LogoutButton from "../component/LogoutButton";
 
 export default async function Home() {
-    async function getUser() {
-        const session = await getSession();
-console.log(session);
+  const logout = async () => {
+    "use server";
+    const session = await getSession();
+    session.destroy();
+    redirect("/");
+  };
+  async function getUser() {
+    const session = await getSession();
+    console.log(session);
     if (session.id) {
-        const user = await db.user.findUnique({ where: { id: session.id } });
-        return user;
-      }
+      const user = await db.user.findUnique({ where: { id: session.id } });
+      return user;
     }
-    const user =  await getUser();
-    console.log("user : ",user);
+  }
+  const user = await getUser();
+  console.log("user : ", user);
 
-    return (
-        <div>
-            <h1>welcome {user?.userName}</h1>
-            <Link href={"/createClub"}>클럽만들기</Link>
-            <button onClick={}>로그아웃</button>
-          
-        </div>
-    )
+  return (
+    <div>
+      <h1>welcome {user?.userName}</h1>
+      <Link href={"/createClub"}>클럽만들기</Link>
+      <form action={logout}>
+        <button>Logout</button>
+      </form>
+    </div>
+  );
 }
