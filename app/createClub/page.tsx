1deb9b0e9e632d2570ed09cfea0accda5fbf07dd
mintonofall <mintonofall@gmail.com>
2025-01-17 17:17:18 +0900

    클럽 생성 페이지에서 상태 관리 변수 이름 변경

--- app/createClub/page.tsx
+++ app/createClub/page.tsx
@@ -6,7 +6,7 @@ import { useActionState } from "react";
 import Link from "next/link";
 
 export default function CreateClub() {
-    const [state, action] = useActionState(handleClubCreate, null);
+    const [_, action] = useActionState(handleClubCreate, null);
     const searchParams = useSearchParams();
     const [clubName, setClubName] = useState("");
     const [location, setLocation] = useState("");
