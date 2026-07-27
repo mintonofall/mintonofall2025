55fed0f8f9c8b31b9ecf98215bd28c5a72082187
mintonofall <mintonofall@gmail.com>
2026-07-27 23:20:27 +0900

    fix database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
+import "dotenv/config";
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-    throw new Error("DATABASE_URL environment variable is not set or is empty.");
+    throw new Error("DATABASE_URL environment variable is not set or is empty.222");
 }
 
 export default {
