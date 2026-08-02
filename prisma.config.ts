ebfd68ff1729c0d7fdcbeb787d47cfa43bf4bff6
mintonofall <mintonofall@gmail.com>
2026-08-02 10:24:35 +0900

    build

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-    throw new Error("DATABASE_URL environment variable is not set or is empty.222");
+    throw new Error("DATABASE_URL environment variable is not set or is empty.");
 }
 
-export default {
+export default defineConfig({
     datasource: {
-        url: databaseUrl,
+        // 타임아웃 방지를 위해 URL 뒤에 ?connect_timeout=30 추가
+        url: databaseUrl.includes("?") ? `${databaseUrl}&connect_timeout=30` : `${databaseUrl}?connect_timeout=30`,
     },
-};
+});
